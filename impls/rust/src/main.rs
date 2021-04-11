82f99f62f867d735247f13d4c129783c893e595c
NKspartan <nkspartan@pop-os.localdomain>
2021-04-10 23:53:54 -0500

    Step 0: Added line editing and history with rustyline

--- impls/rust/src/main.rs
+++ impls/rust/src/main.rs
@@ -1,18 +1,35 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 use std::io::{self, Write};
+
 mod step0_repl;
 
 fn main() {
+    let mut rl = Editor::<()>::new();
+
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+
     loop {
-        let mut input = String::new();
+        let readline = rl.readline("user> ");
 
-        print!("user> ");
-        let _ = io::stdout().flush();
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("{}", step0_repl::rep(&line));
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {}
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
 
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-
-        print!("{}", step0_repl::rep(&input));
-        let _ = io::stdout().flush();
+        rl.save_history("history.txt").unwrap();
     }
 }
